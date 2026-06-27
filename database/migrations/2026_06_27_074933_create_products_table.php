abaa06d0bec6678c64a3794a40a80ec5f66cbe0b
Razanbalata <razanbalata@gmail.com>
2026-06-27 10:59:54 +0300

    Add Product and Supplier management with models, controllers, requests, policies, factories, migrations, and seeders

---
+++ database/migrations/2026_06_27_074933_create_products_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('scientific_name')->nullable();
+            $table->string('sku')->unique();
+            $table->string('barcode')->nullable()->unique();
+            $table->text('description')->nullable();
+            $table->decimal('purchase_price', 10, 2);
+            $table->decimal('selling_price', 10, 2);
+            $table->integer('stock_quantity')->default(0);
+            $table->integer('minimum_stock')->default(10);
+
+            $table->date('expiration_date')->nullable();
+
+            $table->string('batch_number')->nullable();
+
+            $table->string('image')->nullable();
+
+            $table->enum('status', [
+                'active',
+                'inactive',
+                'discontinued'
+            ])->default('active');
+
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
+            $table->foreignId('supplier_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
